e0b69f984ee79083a82948e99199e894de2b47bb
avivahfx <aviva@Weiweis-MacBook-Pro.local>
2019-04-07 10:04:28 -0400

    20190407

--- 225. Implement Stack using Queues.py
+++ 225. Implement Stack using Queues.py
@@ -66,3 +66,12 @@ class MyStack:
         
         return False
 
+'''
+MyStack stack = new MyStack();
+
+stack.push(1);
+stack.push(2);  
+stack.top();   // returns 2
+stack.pop();   // returns 2
+stack.empty(); // returns false
+'''
